1821b72a73d3ffd237db4d6751c88d16c5303f9f
DESKTOP-EKKK335\Dan <danblike@gmail.com>
2018-07-17 19:44:04 -0700

    Fleshed out Fleet entity, fixed .gitignore

--- NeptunesPride/Entities/Report/Fleet.cs
+++ NeptunesPride/Entities/Report/Fleet.cs
@@ -7,10 +7,20 @@ namespace NeptunesWarMachine.Entities
 {
     public class Fleet
     {
+        public int Ouid { get; set; }
         [JsonProperty("uid")]
         public int UniqueId { get; set; }
         public int L { get; set; }
         [JsonProperty("o")]
         public List<Order> Orders { get; set; }
+        [JsonProperty("n")]
+        public string Name { get; set; }
+        public int Puid { get; set; }
+        public int W { get; set; }
+        public double Y { get; set; }
+        public double X { get; set; }
+        public int St { get; set; }
+        public double Lx { get; set; }
+        public double Ly { get; set; }
     }
 }
