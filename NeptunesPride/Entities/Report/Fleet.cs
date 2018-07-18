38a898db99500568e8013015afe9220c878519d8
DESKTOP-EKKK335\Dan <danblike@gmail.com>
2018-07-18 00:34:07 -0700

    Fleets now properly parse Orders

--- NeptunesPride/Entities/Report/Fleet.cs
+++ NeptunesPride/Entities/Report/Fleet.cs
@@ -1,14 +1,17 @@
 ﻿using NeptunesWarMachine.Utils;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace NeptunesWarMachine.Entities
 {
     public class Fleet
     {
-        public int Ouid { get; set; }
+        [JsonProperty("ouid")]
+        public int StarId { get; set; }
         [JsonProperty("uid")]
         public int UniqueId { get; set; }
         [JsonProperty("puid")]
@@ -18,8 +21,10 @@ namespace NeptunesWarMachine.Entities
         [JsonProperty("st")]
         public int ShipsStationed { get; set; }
         [JsonProperty("o")]
+        [JsonConverter(typeof(OrderConverter))]
         public List<Order> Orders { get; set; }
-        public int L { get; set; }
+        [JsonProperty("l")]
+        public bool IsLoopingOrders { get; set; }
         public int W { get; set; }
         public double Y { get; set; }
         public double X { get; set; }
@@ -27,11 +32,38 @@ namespace NeptunesWarMachine.Entities
         public double Ly { get; set; }
     }
 
+    public class Order
+    {
+        public int Delay { get; set; }
+        public int StarId { get; set; }
+        public int Action { get; set; }
+        public int Ships { get; set; }
+    }
+
     public class FleetConverter : CustomJsonConverter<Fleet>
     {
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             return JsonUtils.DeserializeTokens<Fleet>(reader);
+        }
+    }
+
+    public class OrderConverter : CustomJsonConverter<Order>
+    {
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JArray jsonArray = JArray.Load(reader);
+            JEnumerable<JToken> tokens = jsonArray.Children();
+            return tokens.Select(token =>
+            {
+                return new Order
+                {
+                    Delay = token[0].ToObject<int>(),
+                    StarId = token[1].ToObject<int>(),
+                    Action = token[2].ToObject<int>(),
+                    Ships = token[3].ToObject<int>()
+                };
+            }).ToList();
         }
     }
 }
